2f33f04556aa114dea48745d472b2ff8846b0ae4
bobby1321 <36699941+bobby1321@users.noreply.github.com>
2018-11-03 14:37:26 -0400

    Update Pi Side/pi_side/pi_side.ino

--- Pi Side/pi_side/pi_side.ino
+++ Pi Side/pi_side/pi_side.ino
@@ -18,8 +18,8 @@ All text above, and the splash screen below must be included in any redistributi
 
 #include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1305.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1305.h"
 
 // Used for software SPI
 #define OLED_CLK 13
