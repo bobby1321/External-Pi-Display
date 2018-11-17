1beb42888923a36af54f5befe658f49e48d118dd
bobby1321 <36699941+bobby1321@users.noreply.github.com>
2018-11-17 12:41:46 -0500

    Stuff

--- PC Side/Hardware Info Getter/Hardware Info Getter/Program.cs
+++ PC Side/Hardware Info Getter/Hardware Info Getter/Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.IO.Ports;
 using System.Net.Sockets;
 using OpenHardwareMonitor.Hardware;
 namespace Get_CPU_Temp5
 {
     class Program
     {
-        static TcpClient tcpClient;
-        static Stream stream;
+        static SerialPort port;
         public class UpdateVisitor : IVisitor
         {
             public void VisitComputer(IComputer computer)
@@ -26,7 +26,7 @@ namespace Get_CPU_Temp5
             public void VisitSensor(ISensor sensor) { }
             public void VisitParameter(IParameter parameter) { }
         }
-        static void GetSystemInfo()
+        static string[] GetSystemInfo()
         {
             UpdateVisitor updateVisitor = new UpdateVisitor();
             Computer computer = new Computer();
@@ -34,18 +34,12 @@ namespace Get_CPU_Temp5
             computer.CPUEnabled = true;
             computer.GPUEnabled = true;
             computer.Accept(updateVisitor);
-            //String CPU_Temp = "";
-            //String CPU_Speed = "";
-            //String CPU_Load = "";
-            //String GPU_Temp = "";
-            //String GPU_Speed = "";
-            //String GPU_Load = "";
-            float? CPU_Temp = 0;
-            float? CPU_Speed = 0;
-            float? CPU_Load = 0;
-            float? GPU_Temp = 0;
-            float? GPU_Load = 0;
-            float? GPU_Speed = 0;
+            String CPU_Temp = "";
+            String CPU_Speed = "";
+            String CPU_Load = "";
+            String GPU_Temp = "";
+            String GPU_Load = "";
+            String GPU_Speed = "";
             for (int i = 0; i < computer.Hardware.Length; i++)
             {
                 if (computer.Hardware[i].HardwareType == HardwareType.CPU)
@@ -55,39 +49,33 @@ namespace Get_CPU_Temp5
                     for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
                     {
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature && computer.Hardware[i].Sensors[j].Name == "CPU Package")
-                            //CPU_Temp = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " °C" + "\r");
-                            //CPU_Temp = (computer.Hardware[i].Sensors[j].Value.ToString()+ "\r");
-                            CPU_Temp = computer.Hardware[i].Sensors[j].Value;
+                            CPU_Temp = (computer.Hardware[i].Sensors[j].Value.ToString()+ "\r");
+                            //CPU_Temp = computer.Hardware[i].Sensors[j].Value;
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Clock && computer.Hardware[i].Sensors[j].Name != "Bus Speed")
                         {
                             avgClockSum += (float)computer.Hardware[i].Sensors[j].Value;
                             coreTempSensorCount++;
                         }
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Load && computer.Hardware[i].Sensors[j].Name == "CPU Total")
-                            // CPU_Load = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " %" + "\r");
-                            //CPU_Load = (computer.Hardware[i].Sensors[j].Value.ToString()+ "\r");
-                            CPU_Load = computer.Hardware[i].Sensors[j].Value;
+                            CPU_Load = (computer.Hardware[i].Sensors[j].Value.ToString()+ "\r");
+                            //CPU_Load = computer.Hardware[i].Sensors[j].Value;
                     }
-                    //CPU_Speed = ("CPU Clock: " + (avgClockSum / coreTempSensorCount) + " MHz" + "\r");
-                    //CPU_Speed = ((avgClockSum / coreTempSensorCount) + "\r");
-                    CPU_Speed = ((int)avgClockSum / coreTempSensorCount);
+                    CPU_Speed = ((avgClockSum / coreTempSensorCount) + "\r");
+                    //CPU_Speed = ((int)avgClockSum / coreTempSensorCount);
                 }
                 else if (computer.Hardware[i].HardwareType == HardwareType.GpuNvidia)
                 {
                     for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
                     {
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature && computer.Hardware[i].Sensors[j].Name == "GPU Core")
-                            //GPU_Temp = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " °C" + "\r");
-                            //GPU_Temp = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
-                            GPU_Temp = computer.Hardware[i].Sensors[j].Value;
+                            GPU_Temp = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                            //GPU_Temp = computer.Hardware[i].Sensors[j].Value;
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Load && computer.Hardware[i].Sensors[j].Name == "GPU Core")
-                            //GPU_Load = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " %" + "\r");
-                            //GPU_Load = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
-                            GPU_Load = computer.Hardware[i].Sensors[j].Value;
+                            GPU_Load = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                            //GPU_Load = computer.Hardware[i].Sensors[j].Value;
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Clock && computer.Hardware[i].Sensors[j].Name == "GPU Core")
-                            //GPU_Speed = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " MHz" + "\r");
-                            //GPU_Speed = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
-                            GPU_Speed = computer.Hardware[i].Sensors[j].Value;
+                            GPU_Speed = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                            //GPU_Speed = computer.Hardware[i].Sensors[j].Value;
                     }
                 }
                 else if (computer.Hardware[i].HardwareType == HardwareType.GpuAti)
@@ -95,52 +83,40 @@ namespace Get_CPU_Temp5
                     for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
                     {
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature && computer.Hardware[i].Sensors[j].Name == "GPU Core")
-                            //GPU_Temp = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " °C" + "\r");
-                            //GPU_Temp = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
-                            GPU_Temp = computer.Hardware[i].Sensors[j].Value;
+                            GPU_Temp = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                            //GPU_Temp = computer.Hardware[i].Sensors[j].Value;
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Load && computer.Hardware[i].Sensors[j].Name == "GPU Core")
-                            //GPU_Load = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " %" + "\r");
-                            //GPU_Load = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
-                            GPU_Load = computer.Hardware[i].Sensors[j].Value;
+                            GPU_Load = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                            //GPU_Load = computer.Hardware[i].Sensors[j].Value;
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Clock && computer.Hardware[i].Sensors[j].Name == "GPU Core")
-                            //GPU_Speed = (computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + " MHz" + "\r");
-                            //GPU_Speed = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
-                            GPU_Speed = computer.Hardware[i].Sensors[j].Value;
+                            GPU_Speed = (computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                            //GPU_Speed = computer.Hardware[i].Sensors[j].Value;
                     }
                 }
             }
-            //Console.WriteLine(CPU_Temp);
-            //Console.WriteLine(CPU_Speed);
-            //Console.WriteLine(CPU_Load);
-            //Console.WriteLine(GPU_Temp);
-            //Console.WriteLine(GPU_Speed);
-            //Console.WriteLine(GPU_Load);
-            //string[] outputs = {CPU_Temp, CPU_Speed, CPU_Load, GPU_Temp, GPU_Speed, GPU_Load };
-            float[] outputs = {(float)CPU_Temp, (float)CPU_Speed, (float)CPU_Load, (float)GPU_Temp, (float)GPU_Speed, (float)GPU_Load };
-            byte[] bytes = new byte[outputs.Length * sizeof(float)];
-            Buffer.BlockCopy(outputs, 0, bytes, 0, bytes.Length);
-            stream.Write(bytes, 0, bytes.Length);
-            //System.IO.File.WriteAllLines(@"D:\WriteOutputs.txt", outputs);
+            string[] outputs = {CPU_Temp, CPU_Speed, CPU_Load, GPU_Temp, GPU_Speed, GPU_Load };
             computer.Close();
+            return outputs;
         }
         static void Main(string[] args)
         {
             try
             {
-                tcpClient = new TcpClient();
-                tcpClient.Connect("raspberrypi.local", 22);
-                stream = tcpClient.GetStream();
-                Console.WriteLine("Success!");
+                port = new SerialPort("COM5", 115200, Parity.None, 8, StopBits.One);
+                port.Open();
+                while (port.IsOpen)
+                {
+                    string[] strings = GetSystemInfo();
+                    for (int i = 0; i < strings.Length; i++)
+                    {
+                        port.Write(strings[i]);
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
             }
             catch
             {
-                Console.WriteLine("Failed");
-            }
-            while (true)
-            {
-                GetSystemInfo();
-                Console.WriteLine("Sent");
-                System.Threading.Thread.Sleep(1000);
+                Console.Write("Well, that didn't work.");
             }
         }
     }
